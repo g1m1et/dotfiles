53864c8cbe2615645ab6ff2b1a37cb97754a0d26
Satoshi Ebihara <s.ebihara@kakehashi.life>
2021-06-09 02:27:06 +0900

    :bug: add unzip for zinit from"gh-r".

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt install build-essential zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-dev zsh tmux tig jq gpg pass
+sudo apt install build-essential zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-dev zsh tmux tig jq gpg pass unzip
 
