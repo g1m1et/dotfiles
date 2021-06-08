fe03087a9adc4097a8bf05e2d3e29b23c80e68cd
Satoshi Ebihara <kiri@succhiello.net>
2021-06-08 16:33:05 +0900

    :bug: remove unnecessary text.

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt install build-essential zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-devasdf zsh tmux tig jq
+sudo apt install build-essential zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev libreadline-dev zsh tmux tig jq
 
